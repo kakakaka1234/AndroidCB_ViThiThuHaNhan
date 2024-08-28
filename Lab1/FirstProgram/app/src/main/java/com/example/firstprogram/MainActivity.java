4c8a1b79cdf3b737f890b3f1a942754c161466b8
kakakaka1234 <vnhan1708@gmail.com>
2024-08-28 12:09:21 +0700

    Lab1

--- Lab1/FirstProgram/app/src/main/java/com/example/firstprogram/MainActivity.java
+++ Lab1/FirstProgram/app/src/main/java/com/example/firstprogram/MainActivity.java
@@ -1,20 +1,97 @@
 package com.example.firstprogram;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtX, txtY;
+    TextView txtResult;
+    Button btnPlus, btnSubtract, btnMultiply, btnDivide, btnModulo;
+
+    private void initControl() {
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtResult = findViewById(R.id.txtResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnModulo = findViewById(R.id.btnModulo);
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(txtX.getText().toString());
+                double y = Double.parseDouble(txtY.getText().toString());
+                double result = x + y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(txtX.getText().toString());
+                double y = Double.parseDouble(txtY.getText().toString());
+                double result = x - y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(txtX.getText().toString());
+                double y = Double.parseDouble(txtY.getText().toString());
+                double result = x * y;
+                txtResult.setText(String.valueOf(result));
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(txtX.getText().toString());
+                double y = Double.parseDouble(txtY.getText().toString());
+                if (y != 0) {
+                    double result = x / y;
+                    txtResult.setText(String.valueOf(result));
+                } else {
+                    txtResult.setText("Cannot divide by zero");
+                }
+            }
+        });
+
+        btnModulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(txtX.getText().toString());
+                int y = Integer.parseInt(txtY.getText().toString());
+                if (y != 0) {
+                    int result = x % y;
+                    txtResult.setText(String.valueOf(result));
+                } else {
+                    txtResult.setText("Cannot divide by zero");
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        initControl();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
