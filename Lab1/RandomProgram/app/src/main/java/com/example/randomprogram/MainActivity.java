3c3fdd0ef890e1dc4c3a9485e71501c4179b8c72
kakakaka1234 <vnhan1708@gmail.com>
2024-08-28 15:54:33 +0700

    RD

--- Lab1/RandomProgram/app/src/main/java/com/example/randomprogram/MainActivity.java
+++ Lab1/RandomProgram/app/src/main/java/com/example/randomprogram/MainActivity.java
@@ -1,21 +1,79 @@
 package com.example.randomprogram;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.view.View;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] diceImage = new int[]{R.drawable.pic_1, R.drawable.pic_2, R.drawable.pic_3, R.drawable.pic_4, R.drawable.pic_5, R.drawable.pic_6};
+    Button rollButton;
+    ImageView di;
+    int delayTime = 50;
+    int rollAnimation = 20;
+    Random random = new Random();
+    LinearLayout diceContainer;
+    Handler handler = new Handler(Looper.getMainLooper());
+
+    private void rollDice() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < rollAnimation; i++) {
+                    final int diceNumber = random.nextInt(6);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            di.setImageResource(diceImage[diceNumber]);
+                        }
+                    });
+                    try {
+                        Thread.sleep(delayTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int finalDiceNumber = random.nextInt(6);
+                        di.setImageResource(diceImage[finalDiceNumber]);
+                    }
+                });
+            }
+        };
+        new Thread(runnable).start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        rollButton = findViewById(R.id.roll_button);
+        di = findViewById(R.id.di);
+        diceContainer = findViewById(R.id.diceContainer);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    rollDice();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.diceContainer), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
