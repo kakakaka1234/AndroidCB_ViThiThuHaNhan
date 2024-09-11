de11e3af51bace0ebad05b1e6d88fe4b998c8f8f
kakakaka1234 <vnhan1708@gmail.com>
2024-09-11 14:14:28 +0700

    LT2

--- Lab2/AnimalSound/app/src/main/java/com/example/animalsound/Animal.java
+++ Lab2/AnimalSound/app/src/main/java/com/example/animalsound/Animal.java
@@ -1,24 +1,55 @@
 package com.example.animalsound;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Random;
 
 public class Animal extends AppCompatActivity {
+
+    int[] images = {
+            R.drawable.bee,
+            R.drawable.cat,
+            R.drawable.dog,
+            R.drawable.crab,
+            R.drawable.jellyfish,
+            R.drawable.koala,
+            R.drawable.monkey,
+            R.drawable.mouse,
+            R.drawable.turtle
+    };
+
+    int[] backgrounds = {
+            R.color.black,
+            R.color.purple_200,
+            R.color.purple_500,
+            R.color.purple_700,
+            R.color.teal_200,
+            R.color.teal_700,
+            R.color.white
+    };
+
+    ImageView imageView;
+    ConstraintLayout constraintLayout;
+
+    private void getViews() {
+        imageView = findViewById(R.id.imageView);
+        constraintLayout = findViewById(R.id.constraint);
+
+        Random random = new Random();
+        int randomImage = images[random.nextInt(images.length)];
+        int randomBackground = backgrounds[random.nextInt(backgrounds.length)];
+
+        imageView.setImageResource(randomImage);
+        constraintLayout.setBackgroundResource(randomBackground);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_animal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getViews();
     }
 }
--- Lab2/AnimalSound/app/src/main/java/com/example/animalsound/Loading.java
+++ Lab2/AnimalSound/app/src/main/java/com/example/animalsound/Loading.java
@@ -1,24 +1,58 @@
 package com.example.animalsound;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Loading extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textViewPercentage;
+    int progressStatus = 0;
+    Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.item_loading);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        progressBar = findViewById(R.id.progressbar);
+        textViewPercentage = findViewById(R.id.textview);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textViewPercentage.setText(progressStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(35);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (progressStatus >= 100) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(Loading.this, Animal.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 }
